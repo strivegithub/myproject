26d237f00844892208cf8283fad93357b0ca768e
strivegithub <421790835@qq.com>
2016-11-03 11:34:23 +0800

    rename package name

---
+++ src/main/java/com/test/eee/Mpt/App.java
@@ -0,0 +1,34 @@
+package com.test.eee.Mpt;
+
+/**
+ * Hello world!
+ *
+ */
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		DOMConfigurator.configure("resources/config/log4j.xml");
+		Logger logger = Logger.getLogger(App.class);
+		logger.info("info Hello World!");
+		logger.debug("debug Hello World");
+		logger.warn("warn Hello World");
+		logger.error("error Hello World");
+		logger.fatal("fatal Hello World");
+		
+		try {
+			int a = 8;
+			int b = 0;
+			int c = a / b;
+		} catch (Exception e) {
+			// TODO: handle exception
+			logger.fatal("Exp : ", e);
+		}
+
+		
+	}
+	
+	
+}
