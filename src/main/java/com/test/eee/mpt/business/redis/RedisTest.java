09a6e63a86cbbe491bb01d716541bb7a8cb02e28
strivegithub <421790835@qq.com>
2016-12-01 20:25:41 +0800

    redis

--- src/main/java/com/test/eee/mpt/business/redis/RedisTest.java
+++ src/main/java/com/test/eee/mpt/business/redis/RedisTest.java
@@ -2,6 +2,8 @@ package com.test.eee.mpt.business.redis;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import redis.clients.jedis.Jedis;
@@ -34,6 +36,9 @@ public class RedisTest {
 		
 		rds.testObject();
 		
+//		rds.testSet();
+		
+		rds.testMap();
 		
 		Set<String> keySet = rds.js.keys("*");
 		System.out.println("RedisTest.RedisTest() size = " + keySet.size());
@@ -82,5 +87,54 @@ public class RedisTest {
 		A cA = (A)SerializationUtil.deserialize(data);
 		System.out.println("RedisTest.testObject() cA " + cA.toString());
 		
+	}
+	
+	
+	public void testSet()
+	{
+		js.hset("set", "f", "v");
+		js.hset("set", "f", "v");
+		js.hset("set", "f2", "v2");
+		
+		js.hset("set2", "ff2", "vv2");
+		
+		Map<String, String> allMap = js.hgetAll("set");
+		
+		System.out.println("-----------------");
+		
+		Set<Entry<String, String>> allkeySet = allMap.entrySet();
+		for (Iterator iterator = allkeySet.iterator(); iterator.hasNext();) {
+			Entry<String, String> entry = (Entry<String, String>) iterator.next();
+			System.out.println("key = " + entry.getKey() + ", value = " + entry.getValue());
+		}
+		System.out.println("-----------------");
+		
+		Set<String> sets = allMap.keySet();
+		for (Iterator iterator = sets.iterator(); iterator.hasNext();) {
+			String string = (String) iterator.next();
+			System.out.println("RedisTest.testSet() map xxx " + allMap.get(string));
+		}
+		
+		String resString = js.hget("set", "f");
+		System.out.println("RedisTest.testSet() f " + resString);
+		resString = js.hget("set", "f2");
+		System.out.println("RedisTest.testSet() f2 " + resString);
+	}
+	
+	public void testMap()
+	{
+		js.mset("set3", "f");
+		js.mset("set3", "f");
+		js.mset("set3", "f3");
+		js.mset("set4", "f2");
+		
+		js.mset("set5", "ff2");
+		
+		List<String> allMap = js.mget("set3");
+		for (Iterator iterator = allMap.iterator(); iterator.hasNext();) {
+			String string = (String) iterator.next();
+			System.out.println("testMap:  " + string);
+		}
+
 	}
 }
